d28c8f2a216122595476d112c7889923632cc772
Germano <ismaelgermano@icloud.com>
2018-04-27 23:51:10 -0300

    css tosco

--- scripts.js
+++ scripts.js
@@ -6,6 +6,12 @@ let limpar = document.getElementById('limpar');
 
 calcular.onclick = botaoClicado;
 limpar.onclick = botaoLimpar;
+        
+function botaoLimpar(){
+    mensagem.textContent = "";
+    peso.value = "";
+    altura.value = "";
+};
 
 function botaoClicado(){
     let imc = peso.value / (altura.value * altura.value);
@@ -13,21 +19,25 @@ function botaoClicado(){
     if(imc < 18.5){
         mensagem.textContent = imc.toFixed(1) + ' Você está abaixo do peso recomendado.';
     }; 
-    
-    if(imc > 18.5){
-                mensagem.textContent = imc.toFixed(1) + ' Você está acima do peso.';
+
+    if(imc >= 18.5 && imc <= 24.9){
+        mensagem.textContent = imc.toFixed(1) + ' Seu peso está normal.';
     };
+
+    if(imc >= 25.0 && imc <= 29.9){
+        mensagem.textContent = imc.toFixed(1) + ' Você está com sobrepeso.';
+    };
+    
+    if(imc >= 30.0 && imc <= 34.9){
+        mensagem.textContent = imc.toFixed(1) + ' Você está com obesidade grau I.';
+    };
+
+    if(imc >= 35.0 && imc <= 39.9){
+        mensagem.textContent = imc.toFixed(1) + ' Você está com obesidade grau II.';
+    };
+
+    if(imc > 40.0){
+        mensagem.textContent = imc.toFixed(1) + ' Você está com obesidade grau III.';
+    };
+
 };
-
-function botaoLimpar(){
-    mensagem.textContent = "";
-    peso.value = "";
-    altura.value = "";
-}
-  
-    
-    
-
-
-
-
